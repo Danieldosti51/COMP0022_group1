bfd45387cb85cc05c6b2f32065007238805ead9f
Daniel Dosti <43644047+Danieldosti51@users.noreply.github.com>
2021-02-17 14:38:23 +0000

    remove need for manual commands

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,1 @@
-FROM php:8.0.2-fpm
-RUN docker-php-ext-install mysqli && docker-php-ext-enable mysqli
+FROM php:8.0.2-apache
--- schema.sql
+++ schema.sql
@@ -1,3 +1,5 @@
+ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY 'root';
+
 CREATE DATABASE IF NOT EXISTS db;
 USE db;
 
